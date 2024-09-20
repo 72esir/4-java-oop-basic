e70d6be1321dfd3b53ccd1e481502ccf64a722c2
Aleksey Salamatin <asalamatin640@gmail.com>
2024-09-20 21:51:25 +0500

    Саламатин Алексей.task1

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -7,6 +7,23 @@ public class Point {
     int x;
     int y;
 
+    public Point(){
+    }
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    void flip(){
+        int temp = x;
+        x = -y;
+        y = -temp;
+    }
+    double distance(Point point){
+        return Math.sqrt(Math.pow((x - point.x),2) + Math.pow(y - point.y,2));
+    }
+    public String toString(){
+        return "(" + Integer.toString(x) + "," + Integer.toString(y) + ")";
+    }
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
